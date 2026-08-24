686ef952d2b6d5eef93aa9619e550d6d89355667
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-08-24 09:37:00 -0500

    fix(dashboard): add ESLint configuration

---
+++ dashboard/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist/', 'coverage/', 'node_modules/'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
