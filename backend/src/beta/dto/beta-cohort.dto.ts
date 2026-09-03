539279b087ec8b81d9e68b4163af3bf21eb621cd
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-09-03 12:58:04 -0500

    style(beta): format cohort DTOs

--- backend/src/beta/dto/beta-cohort.dto.ts
+++ backend/src/beta/dto/beta-cohort.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsOptional, IsString, Length, MaxLength } from "class-validator";
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Length,
+  MaxLength,
+} from "class-validator";
 
 export class InviteBetaCohortMemberDto {
   @IsEmail()
