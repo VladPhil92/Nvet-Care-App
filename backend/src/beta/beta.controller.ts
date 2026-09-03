2f798132e634acecb731d208ce9d8ecd2d9f9848
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-09-03 06:35:23 -0500

    fix(beta): format beta controller imports

--- backend/src/beta/beta.controller.ts
+++ backend/src/beta/beta.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
 import { UserRole } from "@prisma/client";
 import { Roles } from "../auth/decorators/roles.decorator";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
