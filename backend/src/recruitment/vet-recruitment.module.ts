360febd1c22f560526f823eeb06be71d5ec2d354
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-09-10 06:25:05 -0500

    feat(recruitment): register outreach consent governance

--- backend/src/recruitment/vet-recruitment.module.ts
+++ backend/src/recruitment/vet-recruitment.module.ts
@@ -6,6 +6,8 @@ import {
   VetInvitationPublicController,
 } from "./vet-invitation.controller";
 import { VetInvitationService } from "./vet-invitation.service";
+import { VetOutreachConsentController } from "./vet-outreach-consent.controller";
+import { VetOutreachConsentService } from "./vet-outreach-consent.service";
 import { VetRecruitmentController } from "./vet-recruitment.controller";
 import { VetRecruitmentService } from "./vet-recruitment.service";
 
@@ -15,8 +17,17 @@ import { VetRecruitmentService } from "./vet-recruitment.service";
     VetRecruitmentController,
     VetInvitationAdminController,
     VetInvitationPublicController,
+    VetOutreachConsentController,
   ],
-  providers: [VetRecruitmentService, VetInvitationService],
-  exports: [VetRecruitmentService, VetInvitationService],
+  providers: [
+    VetRecruitmentService,
+    VetInvitationService,
+    VetOutreachConsentService,
+  ],
+  exports: [
+    VetRecruitmentService,
+    VetInvitationService,
+    VetOutreachConsentService,
+  ],
 })
 export class VetRecruitmentModule {}
