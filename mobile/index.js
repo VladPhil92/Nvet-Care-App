434f275814a08e68d64ec07b653bccb93aeb3fef
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-09-21 11:42:48 -0500

    feat(mobile): initialize privacy-safe Sentry observability

--- mobile/index.js
+++ mobile/index.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler'
+import './src/observability/sentry'
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
